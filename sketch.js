d334db23ff4175a6b267169e0c8551da332f7d76
yoichiazegami <64847521+yoichiazegami@users.noreply.github.com>
2024-05-06 17:26:24 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,19 +1,106 @@
-var tex;
-function preload() {
-  tex = loadImage('texture.jpg');
-}
+let img;
+let input;
+let angleX = 0;
+let angleY = 0;
+let cam;
 
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight, WEBGL);
   canvas.position(0, 0);
+  input = createFileInput(handleFile);
+  input.position(10, 10);
+  cam = createCamera();
 }
-
-
 
 function draw() {
   background(220);
-  rotateX(frameCount * 0.01);
-  rotateY(frameCount * 0.01);
-  texture(tex);
-  box(100, 100, 161.4);
+
+  noStroke();
+  
+  if (img) {
+    texture(img);
+    textureWidth = img.width;
+    textureHeight = img.height;
+  }
+  boxWithTexture(200);
+  textureMode(NORMAL);
+}
+
+function boxWithTexture(size) {
+    orbitControl();
+    scale(2);
+    translate(50, 20, 50);
+  
+    push();
+    translate(-100, -100, 0);
+    beginShape();
+    rotateY(0);
+    vertex(0, 0, 0, 0, 0.382);
+    vertex(100, 0, 0, 0.25, 0.382);
+    vertex(100, 161.8, 0, 0.25, 1);
+    vertex(0, 161.8, 0, 0, 1);
+    endShape();
+    pop();
+  
+    push();
+    translate(0, -100, 0);
+    rotateY(PI / 2);
+    beginShape();
+    vertex(0, 0, 0, 0.25, 0.382);
+    vertex(100, 0, 0, 0.5, 0.382);
+    vertex(100, 161.8, 0, 0.5, 1);
+    vertex(0, 161.8, 0, 0.25, 1);
+    endShape();
+    pop();
+  
+    push();
+    translate(0, -100, -100);
+    rotateY(PI * -1);
+    beginShape();
+    vertex(0, 0, 0, 0.5, 0.382);
+    vertex(100, 0, 0, 0.75, 0.382);
+    vertex(100, 161.8, 0, 0.75, 1);
+    vertex(0, 161.8, 0, 0.5, 1);
+    endShape();
+    pop();
+  
+    push();
+    translate(-100, -100, -100);
+    rotateY(PI / 2 * -1);
+    beginShape();
+    vertex(0, 0, 0, 0.75, 0.382);
+    vertex(100, 0, 0, 1, 0.382);
+    vertex(100, 161.8, 0, 1, 1);
+    vertex(0, 161.8, 0, 0.75, 1);
+    endShape();
+    pop();
+  
+    push();
+    translate(-100, -100, 0);
+    rotateX(PI / 2);
+    rotateZ(PI / 2 * -1);
+    beginShape();
+    vertex(0, 0, 0, 0.25, 0);
+    vertex(100, 0, 0, 0.5, 0);
+    vertex(100, 100, 0, 0.5, 0.382);
+    vertex(0, 100, 0, 0.25, 0.382);
+    endShape();
+    pop();
+}
+
+function handleFile(file) {
+  print(file);
+  if (file.type === 'image') {
+    img = createImg(file.data, '');
+    img.hide();
+  } else {
+    img = null;
+  }
+}
+
+function mouseDragged() {
+  let dx = mouseX - pmouseX;
+  let dy = mouseY - pmouseY;
+  angleX += dy * 0.01;
+  angleY += dx * 0.01;
 }
