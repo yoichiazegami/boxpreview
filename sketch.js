9c11e5a3b6c7d99baf02154a3be7d6a3e7f0cf94
yoichiazegami <64847521+yoichiazegami@users.noreply.github.com>
2024-05-08 15:57:56 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,123 +3,102 @@ let input;
 let angleX = 0;
 let angleY = 0;
 let cam;
-let sliderBack;
-let sliderLight;
-let sliderAmb;
-let light;
+let slider;
 
 function setup() {
-  let canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+  let canvas = createCanvas(windowWidth, windowHeight-40, WEBGL);
   canvas.position(0, 0);
   input = createFileInput(handleFile);
-  input.position(10, 100);
+  input.position(10, 10);
   cam = createCamera();
-
-  sliderBack = createSlider(0, 255, 220);
-  sliderBack.position(15, windowHeight-35);
-  sliderLight = createSlider(0, 25, 15);
-  sliderLight.position(15, windowHeight-60);
-  sliderAmb = createSlider(0, 25, 20);
-  sliderAmb.position(15, windowHeight-85);
+  slider = createSlider(0, 255, 40);
+  slider.position(10, windowHeight-35);
 }
 
 function draw() {
-  orbitControl(1.7, 1.7, 0.03);
-  background(sliderBack.value());
+  background(255-slider.value());
   noStroke();
   if (img) {
     texture(img);
     textureWidth = img.width;
     textureHeight = img.height;
   }
-  boxWithTexture();
+  boxWithTexture(200);
   textureMode(NORMAL);
 }
 
+function boxWithTexture(size) {
+    orbitControl();
+    scale(2);
+    translate(50, 20, 50);
 
-function boxWithTexture() {
+    push();
+    translate(-100, -100, 0);
+    beginShape();
+    rotateY(0);
+    vertex(0, 0, 0, 0, 0.276);
+    vertex(100, 0, 0, 0.25, 0.276);
+    vertex(100, 161.8, 0, 0.25, 0.724);
+    vertex(0, 161.8, 0, 0, 0.724);
+    endShape();
+    pop();
 
-  light = 25 - sliderLight.value();
-  amb = 125 + sliderAmb.value();
-  ambientLight(amb);
+    push();
+    translate(0, -100, 0);
+    rotateY(PI / 2);
+    beginShape();
+    vertex(0, 0, 0, 0.25, 0.276);
+    vertex(100, 0, 0, 0.5, 0.276);
+    vertex(100, 161.8, 0, 0.5, 0.724);
+    vertex(0, 161.8, 0, 0.25, 0.724);
+    endShape();
+    pop();
 
-  directionalLight(152 - light*0, 152 - light*0, 152 - light*0, 0, 1, 0);//1
-  directionalLight(152 - light*1, 152 - light*1, 152 - light*1, -1, 0, 0);//2
-  directionalLight(152 - light*2, 152 - light*2, 152 - light*2, 0, 0, 1);//3
-  directionalLight(152 - light*1, 152 - light*1, 152 - light*1, 1, 0, 0);//4
-  directionalLight(152 - light*2, 152 - light*2, 152 - light*2, 0, 0, -1);//5
-  directionalLight(152 - light*3, 140 - light*3, 152 - light*3, 0, -1, 0);//6
+    push();
+    translate(0, -100, -100);
+    rotateY(PI * -1);
+    beginShape();
+    vertex(0, 0, 0, 0.5, 0.276);
+    vertex(100, 0, 0, 0.75, 0.276);
+    vertex(100, 161.8, 0, 0.75, 0.724);
+    vertex(0, 161.8, 0, 0.5, 0.724);
+    endShape();
+    pop();
 
-  scale(1.9);
-  translate(50, 20, 50);
+    push();
+    translate(-100, -100, -100);
+    rotateY(PI / 2 * -1);
+    beginShape();
+    vertex(0, 0, 0, 0.75, 0.276);
+    vertex(100, 0, 0, 1, 0.276);
+    vertex(100, 161.8, 0, 1, 0.724);
+    vertex(0, 161.8, 0, 0.75, 0.724);
+    endShape();
+    pop();
 
-  push();
-  translate(-100, -100, 0);
-  beginShape();
-  rotateY(0);
-  vertex(0, 0, 0, 0, 0.382);
-  vertex(100, 0, 0, 0.25, 0.382);
-  vertex(100, 161.8, 0, 0.25, 1);
-  vertex(0, 161.8, 0, 0, 1);
-  endShape();
-  pop();
+    push();
+    translate(-100, -100, 0);
+    rotateX(PI / 2);
+    rotateZ(PI / 2 * -1);
+    beginShape();
+    vertex(0, 0, 0, 0.25, 0);
+    vertex(100, 0, 0, 0.5, 0);
+    vertex(100, 100, 0, 0.5, 0.276);
+    vertex(0, 100, 0, 0.25, 0.276);
+    endShape();
+    pop();
 
-  push();
-  translate(0, -100, 0);
-  rotateY(PI / 2);
-  beginShape();
-  vertex(0, 0, 0, 0.25, 0.382);
-  vertex(100, 0, 0, 0.5, 0.382);
-  vertex(100, 161.8, 0, 0.5, 1);
-  vertex(0, 161.8, 0, 0.25, 1);
-  endShape();
-  pop();
-
-  push();
-  translate(0, -100, -100);
-  rotateY(PI * -1);
-  beginShape();
-  vertex(0, 0, 0, 0.5, 0.382);
-  vertex(100, 0, 0, 0.75, 0.382);
-  vertex(100, 161.8, 0, 0.75, 1);
-  vertex(0, 161.8, 0, 0.5, 1);
-  endShape();
-  pop();
-
-  push();
-  translate(-100, -100, -100);
-  rotateY(PI / 2 * -1);
-  beginShape();
-  vertex(0, 0, 0, 0.75, 0.382);
-  vertex(100, 0, 0, 1, 0.382);
-  vertex(100, 161.8, 0, 1, 1);
-  vertex(0, 161.8, 0, 0.75, 1);
-  endShape();
-  pop();
-
-  push();
-  translate(-100, -100, 0);
-  rotateX(PI / 2);
-  rotateZ(PI / 2 * -1);
-  beginShape();
-  vertex(0, 0, 0, 0.25, 0);
-  vertex(100, 0, 0, 0.5, 0);
-  vertex(100, 100, 0, 0.5, 0.382);
-  vertex(0, 100, 0, 0.25, 0.382);
-  endShape();
-  pop();
-
-  push();
-  translate(0, 61.8, -100);
-  rotateX(PI / 2);
-  rotateY(PI);
-  beginShape();
-  vertex(0, 0, 0, 0.75, 0);
-  vertex(100, 0, 0, 1, 0);
-  vertex(100, 100, 0, 1, 0.382);
-  vertex(0, 100, 0, 0.75, 0.382);
-  endShape();
-  pop();
+    push();
+    translate(0, 61.8, -100);
+    rotateX(PI / 2);
+    rotateY(PI);
+    beginShape();
+    vertex(0, 0, 0, 0.5, 0.724);
+    vertex(100, 0, 0, 0.75, 0.724);
+    vertex(100, 100, 0, 0.75, 1);
+    vertex(0, 100, 0, 0.5, 1);
+    endShape();
+    pop();
 }
 
 function handleFile(file) {
@@ -130,4 +109,11 @@ function handleFile(file) {
   } else {
     img = null;
   }
+}
+
+function mouseDragged() {
+  let dx = mouseX - pmouseX;
+  let dy = mouseY - pmouseY;
+  angleX += dy * 0.01;
+  angleY += dx * 0.01;
 }
